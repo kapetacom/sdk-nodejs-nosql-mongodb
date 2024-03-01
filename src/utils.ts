2f346391e3a95f737fd64bfe32388a028133efc4
Morten Holm <mohoholm@gmail.com>
2024-03-01 11:51:38 +0100

    fix: review comments

--- src/utils.ts
+++ src/utils.ts
@@ -4,19 +4,16 @@
  */
 
 import {ConfigProvider} from "@kapeta/sdk-config";
+
 export const RESOURCE_TYPE = 'kapeta/resource-type-mongodb';
 export const PORT_TYPE = 'mongodb';
 export async function createDBURI(provider:ConfigProvider, resourceName: string) {
     const dbInfo = await provider.getResourceInfo(RESOURCE_TYPE, PORT_TYPE, resourceName);
-    console.log("resourceName", resourceName, dbInfo);
     if (!dbInfo) {
         throw new Error(`Resource ${resourceName} not found`);
     }
-    const dbName =
-        dbInfo.options && dbInfo.options.dbName
-            ? dbInfo.options.dbName
-            : resourceName;
-    const protocol = dbInfo.options && dbInfo.options.protocol ? dbInfo.options.protocol : 'mongodb';
+    const dbName = dbInfo.options?.dbName ?? resourceName;
+    const protocol = dbInfo?.options?.protocol ?? 'mongodb';
 
     let credentials = ''
     if (dbInfo.credentials?.username) {
@@ -28,9 +25,7 @@ export async function createDBURI(provider:ConfigProvider, resourceName: string)
     }
 
     if (protocol == 'mongodb+srv') {
-        const result = `mongodb+srv://${credentials}@${dbInfo.host}/${encodeURIComponent(dbName)}?authSource=admin`;
-        console.log("mongo db url", result);
-        return result;
+        return `mongodb+srv://${credentials}@${dbInfo.host}/${encodeURIComponent(dbName)}?authSource=admin`;
     } else {
         return `mongodb://${credentials}@${dbInfo.host}:${dbInfo.port}/${encodeURIComponent(dbName)}?authSource=admin&directConnection=true`;
     }
