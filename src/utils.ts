7ec1c90ab2785516d77ec41aea41141ecd526624
Morten Holm <mohoholm@gmail.com>
2024-03-01 08:59:52 +0100

    feat: add support for mongodb+srv protocol

--- src/utils.ts
+++ src/utils.ts
@@ -8,6 +8,7 @@ export const RESOURCE_TYPE = 'kapeta/resource-type-mongodb';
 export const PORT_TYPE = 'mongodb';
 export async function createDBURI(provider:ConfigProvider, resourceName: string) {
     const dbInfo = await provider.getResourceInfo(RESOURCE_TYPE, PORT_TYPE, resourceName);
+    console.log("resourceName", resourceName, dbInfo);
     if (!dbInfo) {
         throw new Error(`Resource ${resourceName} not found`);
     }
@@ -15,6 +16,7 @@ export async function createDBURI(provider:ConfigProvider, resourceName: string)
         dbInfo.options && dbInfo.options.dbName
             ? dbInfo.options.dbName
             : resourceName;
+    const protocol = dbInfo.options && dbInfo.options.protocol ? dbInfo.options.protocol : 'mongodb';
 
     let credentials = ''
     if (dbInfo.credentials?.username) {
@@ -25,5 +27,11 @@ export async function createDBURI(provider:ConfigProvider, resourceName: string)
         }
     }
 
-    return `mongodb://${credentials}@${dbInfo.host}:${dbInfo.port}/${encodeURIComponent(dbName)}?authSource=admin&directConnection=true`;
+    if (protocol == 'mongodb+srv') {
+        const result = `mongodb+srv://${credentials}@${dbInfo.host}/${encodeURIComponent(dbName)}?authSource=admin`;
+        console.log("mongo db url", result);
+        return result;
+    } else {
+        return `mongodb://${credentials}@${dbInfo.host}:${dbInfo.port}/${encodeURIComponent(dbName)}?authSource=admin&directConnection=true`;
+    }
 }
